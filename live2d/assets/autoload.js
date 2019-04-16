92554c86d01f954d29d1be72615b51f26525c5d2
yuDuChen <49541714+yuDuChen@users.noreply.github.com>
2019-04-16 17:20:28 +0800

    Update autoload.js

--- live2d/assets/autoload.js
+++ live2d/assets/autoload.js
@@ -4,7 +4,7 @@ try {
     $.ajax({url: 'assets/waifu-tips.min.js?v=1.4.2',dataType:"script", cache: true, async: false});
     $.ajax({url: 'assets/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
     /* 可直接修改部分参数 */
-    live2d_settings['hitokotoAPI'] = 'hitokoto.cn';  // 一言 API
+    live2d_settings['hitokotoAPI'] = 'hitokoto.cn';  // 一言 API测试
     live2d_settings['modelId'] = 5;                  // 默认模型 ID
     live2d_settings['modelTexturesId'] = 1;          // 默认材质 ID
     live2d_settings['modelStorage'] = false;         // 不储存模型 ID
